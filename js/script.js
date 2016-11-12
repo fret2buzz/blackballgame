d0ee4369aaa4d596605cb9631677ddc8607329ce
fret2buzz <fret2buzz@hotmail.com>
2016-11-12 06:15:38 +0200

    reset button and various enhancements

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-function myClass(id, ball) {
-
+function myClass(id, buttons) {
   this.container = document.getElementById(id);
-  this.ball = document.getElementById(ball);
+  this.ball = document.querySelectorAll('#' + id +' .ball')[0];
   this.containerWidth = this.container.offsetWidth;
   this.containerHeight = this.container.offsetHeight;
   this.ballLeft = 0;
@@ -9,11 +8,18 @@ function myClass(id, ball) {
 
   this.ballWidth = this.ball.offsetWidth;
   this.move = 0;
+  
+  this.initialX = getCssProperty(this.ball, "left");
+  this.initialY = getCssProperty(this.ball, "top");
+
   this.moveTop = getCssProperty(this.ball, "top");
   this.items = this.container.getElementsByClassName("cell");
   this.itemsLength = this.items.length;
   this.flag = 0;
-  
+  this.resetButton = document.querySelectorAll('#' + buttons +' .reset')[0];
+  this.random = Math.floor((Math.random() * 6) + 1);
+  this.row = 11;
+  this.pos = 61;
 }
 
 myClass.prototype.initialize = function() {
@@ -21,20 +27,50 @@ myClass.prototype.initialize = function() {
   console.log(this.container);
   console.log(this.items);
   console.log(this.itemsLength);
+  console.log(this.resetButton);
 
   // Listen for a transition
   var transitionEvent = whichTransitionEvent();
   transitionEvent && ball.addEventListener(transitionEvent, function() {
     self.flag = 0;
-    console.log('Transition complete!  This is the callback, no library needed!');
+    // console.log('Transition complete!  This is the callback, no library needed!');
   });
 
-  this.step();
+  for(var i = 0; i < this.itemsLength; i++) {
+    this.items[i].setAttribute('data-index', i);
+    this.items[i].addEventListener('click', this.moveFunc.bind(this), false);
+  }
+
+  this.resetButton.addEventListener('click', this.reset.bind(this), false);
+}
+
+myClass.prototype.reset = function() {
+  for(var i = 0; i < this.itemsLength; i++) {
+    this.items[i].className = "cell";
+  }
+  this.random = Math.floor((Math.random() * 6) + 1);
+  this.ball.className = "ball";
+  this.ball.style.left = this.initialX + "px";
+  this.ball.style.top = this.initialY + "px";
 }
 
 myClass.prototype.getPos = function() {
   this.ballLeft = getCssProperty(this.ball, "left");
   this.ballTop = getCssProperty(this.ball, "top");
+  console.log("x: ", this.ballLeft, " y: ", this.ballTop);
+  var x = 0;
+  var y = 0;
+  if(this.ballLeft % this.ballWidth != 0){
+    x = (this.ballLeft - this.ballWidth + (this.ballWidth / 2)) / this.ballWidth;
+  } else {
+    x = this.ballLeft / this.ballWidth;
+  }
+  y = this.ballTop / this.ballWidth;
+  // console.log(x);
+  // console.log(y);
+  this.pos = (y * this.row) + x;
+  console.log(this.pos);
+  this.items[this.pos].className = this.items[this.pos].className + " punk";
 }
 
 myClass.prototype.setPos = function() {
@@ -42,114 +78,104 @@ myClass.prototype.setPos = function() {
   this.ball.style.top = this.moveTop + "px";
 }
 
-myClass.prototype.step = function() {
-  var self = this;
-
-  var moveRight = function() {
-    console.log("right");
-    self.move = self.ballLeft + self.ballWidth;
-    self.ball.style.left = self.move + "px";
-    console.log(self.move, self.moveTop);
-    if(self.move == self.containerWidth) {
-      console.log("You lose");
-    }
+myClass.prototype.moveRight = function() {
+  console.log("right");
+  this.move = this.ballLeft + this.ballWidth;
+  this.ball.style.left = this.move + "px";
+  // console.log(this.move, this.moveTop);
+  if(this.move == this.containerWidth) {
+    console.log("You lose");
   }
+}
 
-  var moveLeft = function(){
-    console.log("left");
-    self.move = self.ballLeft - self.ballWidth;
-    self.ball.style.left = self.move + "px";
-    console.log(self.move, self.moveTop);
-    if(self.move < 0) {
-      console.log("You lose");
-    }
-  };
-
-  // top right
-  var moveRightTop = function(){
-    console.log("right top");
-    self.move = self.ballLeft + (self.ballWidth / 2);
-    self.moveTop = self.ballTop - self.ballWidth;
-    self.setPos();
-    console.log(self.moveTop, self.moveTop);
-    if(self.moveTop < 0 || self.move == self.containerWidth) {
-      console.log("You lose");
-    }
-  };
-
-  // bottom left
-  var moveLeftBottom = function(){
-    console.log("left bottom");
-    self.move = self.ballLeft - (self.ballWidth / 2);
-    self.moveTop = self.ballTop + self.ballWidth;
-    self.setPos();
-    console.log(self.move, self.moveTop);
-    if(self.moveTop == self.containerHeight) {
-      console.log("You lose");
-    }
-  };
-
-  // bottom right
-  var moveRightBottom = function(){
-    console.log("right bottom");
-    self.move = self.ballLeft + (self.ballWidth / 2);
-    self.moveTop = self.ballTop + self.ballWidth;
-    self.setPos();
-    console.log(self.move, self.moveTop);
-    if(self.moveTop == self.containerHeight) {
-      console.log("You lose");
-    }
-  };
-
-  var moveLeftTop = function(){
-    console.log("left top");
-    self.move = self.ballLeft - (self.ballWidth / 2);
-    self.moveTop = self.ballTop - self.ballWidth;
-    self.setPos();
-    console.log(self.move, self.moveTop);
-    if(self.moveTop < 0 || self.move < 0) {
-      console.log("You lose");
-      // self.ball.className = "ball hidden";
-    }
-  };
-  
-  var random = Math.floor((Math.random() * 6) + 1);
-
-  for(var i = 0; i < this.itemsLength; i++) {
-    this.items[i].addEventListener('click', function(e){
-      if(self.flag == 0) {
-        var curCell = e.currentTarget;
-        if(curCell.className.indexOf("active") == -1){
-          curCell.className = "cell active";
-          self.flag = 1;
-          self.getPos();
-          
-          switch(random) {
-            case 1:
-              moveRightTop();
-              break;
-            case 2:
-              moveRight();
-              break;
-            case 3:
-              moveRightBottom();
-              break;
-            case 4:
-              moveLeftBottom();
-              break;
-            case 5:
-              moveLeft();
-              break;
-            case 6:
-              moveLeftTop();
-              break;
-          };
-        };
-      };
-    }, false);
+myClass.prototype.moveLeft = function(){
+  console.log("left");
+  this.move = this.ballLeft - this.ballWidth;
+  this.ball.style.left = this.move + "px";
+  // console.log(this.move, this.moveTop);
+  if(this.move < 0) {
+    console.log("You lose");
   }
-  
 };
+
+myClass.prototype.moveRightTop = function(){
+  console.log("right top");
+  this.move = this.ballLeft + (this.ballWidth / 2);
+  this.moveTop = this.ballTop - this.ballWidth;
+  this.setPos();
+  // console.log(this.moveTop, this.moveTop);
+  if(this.moveTop < 0 || this.move == this.containerWidth) {
+    console.log("You lose");
+  }
+};
+
+myClass.prototype.moveLeftBottom = function(){
+  console.log("left bottom");
+  this.move = this.ballLeft - (this.ballWidth / 2);
+  this.moveTop = this.ballTop + this.ballWidth;
+  this.setPos();
+  // console.log(this.move, this.moveTop);
+  if(this.moveTop == this.containerHeight) {
+    console.log("You lose");
+  }
+};
+
+myClass.prototype.moveRightBottom = function(){
+  console.log("right bottom");
+  this.move = this.ballLeft + (this.ballWidth / 2);
+  this.moveTop = this.ballTop + this.ballWidth;
+  this.setPos();
+  // console.log(this.move, this.moveTop);
+  if(this.moveTop == this.containerHeight) {
+    console.log("You lose");
+  }
+};
+
+myClass.prototype.moveLeftTop = function(){
+  console.log("left top");
+  this.move = this.ballLeft - (this.ballWidth / 2);
+  this.moveTop = this.ballTop - this.ballWidth;
+  this.setPos();
+  // console.log(this.move, this.moveTop);
+  if(this.moveTop < 0 || this.move < 0) {
+    console.log("You lose");
+    // this.ball.className = this.ball.className + " hidden";
+  }
+};
+
+myClass.prototype.moveFunc = function(e) {
+  if(this.flag == 0) {
+    var curCell = e.currentTarget;
+    var index = parseInt(curCell.getAttribute("data-index"));
+    console.log("index: ", index);
+    if(curCell.className.indexOf("active") == -1){
+      curCell.className = curCell.className + " active";
+      this.flag = 1;
+      this.getPos();
+      switch(this.random) {
+        case 1:
+          this.moveRightTop();
+          break;
+        case 2:
+          this.moveRight();
+          break;
+        case 3:
+          this.moveRightBottom();
+          break;
+        case 4:
+          this.moveLeftBottom();
+          break;
+        case 5:
+          this.moveLeft();
+          break;
+        case 6:
+          this.moveLeftTop();
+          break;
+      };
+      
+    };
+  };
+}
 
 /* From Modernizr */
 function whichTransitionEvent(){
@@ -174,7 +200,7 @@ function getCssProperty(elem, property){
 }
 
 window.onload = function () {
-  var game = new myClass("container", "ball");
+  var game = new myClass("container", "buttons");
   game.initialize();
 }
 
