8e3983f46cdb9682832d922e82e92387d5991647
fret2buzz <fret2buzz@hotmail.com>
2016-11-14 20:15:38 +0200

    Dijkstra's algorithm

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-
-
 function myClass(id, buttons) {
   this.container = document.getElementById(id);
+  this.containerWidth = this.container.offsetWidth;
+  this.containerHeight = this.container.offsetHeight;
   this.containerPositionLeft = 0;
   this.containerPositionTop = 0;
   
   this.ball = document.querySelectorAll('#' + id +' .ball')[0];
-  
-  this.containerWidth = this.container.offsetWidth;
-  this.containerHeight = this.container.offsetHeight;
-  
   this.ballLeft = 0;
   this.ballTop = 0;
   this.x = 0;
@@ -29,21 +25,19 @@ function myClass(id, buttons) {
   this.transitionFlag = 0;
   this.resetButton = document.querySelectorAll('#' + buttons +' .reset')[0];
   this.possibleMovesButton = document.querySelectorAll('#' + buttons +' .possible-moves')[0];
+
   this.end = 0;
   this.stop = 0;
   this.row = 11;
   this.pos = 60;
-  this.prev = this.pos;
-  this.newArray = [];
-
+  this.map = [];
+  this.mapItem = {};
+  this.path = [];
 }
+
 
 myClass.prototype.initialize = function() {
   var self = this;
-  // console.log(this.container);
-  // console.log(this.items);
-  // console.log(this.itemsLength);
-  // console.log(this.resetButton);
 
   // Listen for a transition
   var transitionEvent = whichTransitionEvent();
@@ -56,13 +50,14 @@ myClass.prototype.initialize = function() {
     this.items[i].setAttribute('data-index', i);
     this.items[i].addEventListener('click', this.moveFunc.bind(this), false);
   }
-  for(var k = 1; k < this.linesLength; k++) {
+  for(var k = 0; k < this.linesLength; k++) {
     this.lines[k].setAttribute('data-index', k);
   }
   this.randomActive();
   this.resetButton.addEventListener('click', this.reset.bind(this), false);
   this.possibleMovesButton.addEventListener('click', this.possibleMoves.bind(this), false);
   this.ball.setAttribute("data-pos", this.pos);
+
 }
 
 myClass.prototype.reset = function() {
@@ -93,12 +88,12 @@ myClass.prototype.randomActive = function() {
 myClass.prototype.getPos = function() {
   this.ballLeft = getCssProperty(this.ball, "left");
   this.ballTop = getCssProperty(this.ball, "top");
-  console.log(this.ballLeft, this.ballTop);
+
 }
 
 myClass.prototype.setPos = function(a) {
   if(this.stop == 0) {
-    console.log(a);
+
     this.containerPositionLeft = this.container.getBoundingClientRect().left;
     this.containerPositionTop = this.container.getBoundingClientRect().top;
     var cellPosition = this.items[a].getBoundingClientRect();
@@ -113,14 +108,8 @@ myClass.prototype.setPos = function(a) {
   this.ball.style.top = this.y + "px";
 }
 
-// myClass.prototype.prevFunc = function(){
-//   if(this.items[this.pos].className.indexOf("active") > 0){
-//     this.pos = this.prev;
-//     this.transitionFlag = 0;
-//   }
-// };
-
 myClass.prototype.loseFunc = function(a){
+  console.log(a);
   var loseFuncRight = ((a + 1) % this.row == 0);
   var loseFuncLeft = (a % this.row == 0 || a == 0);
   var loseFuncTop = (a / this.row < 1);
@@ -144,100 +133,28 @@ myClass.prototype.loseFunc = function(a){
     if(loseFuncBottom){
       this.y = this.ballTop + this.ballWidth;
     }
+    
     this.ball.className = this.ball.className + " hidden";
 
   } else {
-    // getting the last row or col. this is the end
-    // if(dir == "right"){
-      if(loseFuncRight){
-        console.log("hor last");
-        this.end = 1;
-      };
-    // }
-    // if(dir == "left"){
-      if(loseFuncLeft){
-        console.log("hor first");
-        this.end = 1;
-      };
-    // }
-    // if(dir.indexOf("top") > 0){
-      if(loseFuncTop){
-        console.log("vert first");
-        this.end = 1;
-      };
-    // }
-    // if(dir.indexOf("bottom") > 0){
-      if(loseFuncBottom){
-        console.log("vert last");
-        this.end = 1;
-      };
-    // }
+    // getting the last row or col. it will be the end
+    if(loseFuncRight){
+      console.log("hor last");
+      this.end = 1;
+    };
+    if(loseFuncLeft){
+      console.log("hor first");
+      this.end = 1;
+    };
+    if(loseFuncTop){
+      console.log("vert first");
+      this.end = 1;
+    };
+    if(loseFuncBottom){
+      console.log("vert last");
+      this.end = 1;
+    };
   }
-}
-
-
-myClass.prototype.getNextIndex = function(dir){
-  if(this.end == 0) {
-
-    var parentIndex = parseInt(this.items[this.pos].parentNode.getAttribute("data-index"));
-    // console.log("------------------------------------");
-    // console.log("parentIndex", parentIndex);
-
-    if(dir == "left"){
-      this.prev = this.pos;
-      this.pos = this.pos - 1;
-      // this.prevFunc();
-    }
-    if(dir == "right"){
-      this.prev = this.pos;
-      this.pos = this.pos + 1;
-      // this.prevFunc();
-    }
-    if(dir == "left top"){
-      this.prev = this.pos;
-      if(parentIndex % 2 == 0){
-        this.pos = this.pos - this.row - 1;
-      } else {
-        this.pos = this.pos - this.row;
-      }
-      // this.prevFunc();
-    }
-    if(dir == "right top"){
-      this.prev = this.pos;
-      if(parentIndex % 2 == 0){
-        this.pos = this.pos - this.row;
-      } else {
-        this.pos = this.pos - this.row + 1;
-      }
-      // this.prevFunc();
-    }
-    if(dir == "left bottom"){
-      this.prev = this.pos;
-      if(parentIndex % 2 == 0){
-        this.pos = this.pos + this.row - 1;
-      } else {
-        this.pos = this.pos + this.row;
-      }
-      // this.prevFunc();
-    }
-    if(dir == "right bottom"){
-      this.prev = this.pos;
-      if(parentIndex % 2 == 0){
-        this.pos = this.pos + this.row;
-      } else {
-        this.pos = this.pos + this.row + 1;
-      }
-      // this.prevFunc();
-    }
-    // console.log("this.pos", this.pos);
-
-  }
-};
-
-myClass.prototype.move = function(a) {
-  this.loseFunc(a);
-  this.setPos(a);
-  // console.log(a);
 }
 
 myClass.prototype.moveFunc = function(e) {
@@ -247,67 +164,91 @@ myClass.prototype.moveFunc = function(e) {
     this.pos = parseInt(this.ball.getAttribute("data-pos"));
     var curCell = e.currentTarget;
     // var index = parseInt(curCell.getAttribute("data-index"));
-    // console.log("index: ", index);
     if(curCell.className.indexOf("active") == -1){
       curCell.className = curCell.className + " active";
       this.transitionFlag = 1;
 
       this.possibleMoves();
-      console.log(this.newArray);
-      if(this.newArray.length == 0){
+      if(this.end == 0){
+        this.path = graph.findShortestPath('cell-' + this.pos, 'cell-120');
+      }
+        
+      var movePoint = parseInt(this.path[1].split("-")[1]);
+      console.log(movePoint);
+
+      console.log(this.path);
+      if(this.path.length == 0){
         console.log("You won");
         this.stop = 1;
       }
 
-      console.log("this.prev: ", this.prev);
-      this.move(this.newArray[Math.floor(Math.random() * this.newArray.length)]);
+      this.loseFunc(movePoint);
+      this.setPos(movePoint);
 
     };
   };
 }
 
-myClass.prototype.check = function(a){
-  this.pos = parseInt(this.ball.getAttribute("data-pos"));
-  this.getNextIndex(a);
-  
-  if(this.items[this.pos].className.indexOf("active") == -1){
-    // this.items[this.pos].className = this.items[this.pos].className + " punk";
-    this.newArray.push(this.pos);
-  }
-};
 
-myClass.prototype.possibleMoves = function(e) {
-  // console.log(this.pos);
-  for(var i = 0; i < this.itemsLength; i++) {
-    // this.items[i].className = this.items[i].className.replace(" punk", "");
-  }
-  
-  if(this.end == 0) {
-    this.newArray = [];
-    for(var i = 1; i < 7; i++){
-      switch(i){
-        case 1:
-          this.check("left");
-          break;
-        case 2:
-          this.check("right");
-          break;
-        case 3:
-          this.check("left top");
-          break;
-        case 4:
-          this.check("right top");
-          break;
-        case 5:
-          this.check("left bottom");
-          break;
-        case 6:
-          this.check("right bottom");
-          break;
+myClass.prototype.possibleMoves = function() {
+  if(this.end == 0){
+    for(var i = 0; i < this.itemsLength; i++){
+      this.mapItem = {};
+      var position = 0;
+      var parentIndex = parseInt(this.items[i].parentNode.getAttribute("data-index"));
+      for(z = 1; z < 7; z++){
+        // left
+        if(z == 1){position = i - 1;}
+        // right
+        if(z == 2){position = i + 1;}
+        // left top
+        if(z == 3){
+          if(parentIndex % 2 == 0){
+            position = i - this.row - 1;
+          } else {
+            position = i - this.row;
+          }
+        }
+        // right top
+        if(z == 4){
+          if(parentIndex % 2 == 0){
+            position = i - this.row;
+          } else {
+            position = i - this.row + 1;
+          }
+        }
+        // left bottom
+        if(z == 5){
+          if(parentIndex % 2 == 0){
+            position = i + this.row - 1;
+          } else {
+            position = i + this.row;
+          }
+        }
+        // right bottom
+        if(z == 6){
+          if(parentIndex % 2 == 0){
+            position = i + this.row;
+          } else {
+            position = i + this.row + 1;
+          }
+        }
+
+        if(this.items[position] != undefined){
+          if(this.items[position].className.indexOf("active") == -1){
+            // this.items[position].className = this.items[position].className + " punk";
+            this.mapItem["cell-"+position] = 1;
+          }
+        }
       }
+      this.map["cell-"+i] = this.mapItem;
     }
     
-  };
+    
+    // console.log(this.map);
+
+    graph = new Graph(this.map);
+  }
 }
 
 /* From Modernizr */
@@ -336,5 +277,4 @@ window.onload = function () {
   var game = new myClass("container", "buttons");
   game.initialize();
 }
-
 
